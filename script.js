dadc41881c522b20c50a9219a682fddb1a27d89e
prajnarbhat <rbhatprajna@gmail.com>
2025-03-07 15:07:15 +0530

    Updated styles, disabled past dates in due date picker, added close button to popup dialog, and implemented input form validation.

--- script.js
+++ script.js
@@ -8,19 +8,33 @@ const taskName = document.getElementById("task");
 const taskDescription = document.getElementById("description");
 const taskDueDate = document.getElementById("dueDate");
 let popup = document.getElementById('popup');
+const zeroStateElement = document.getElementById('zero-state-container');
+const searchResultEmptyStateElement = document.getElementById('search-result-empty');
 
-
-function addNewTask(){
+function addNewTask() {
   submitForm.style.display = 'block';
 }
-function closeForm(){
+
+function closeForm() {
   submitForm.style.display = 'none';
 }
+
 // Clears input fields 
 function resetForm() {
   taskName.value = "";
   taskDescription.value = "";
   taskDueDate.value = "";
+}
+
+// Disabling the past dates by setting an attribute as minimum date as today
+function disablePastDates() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+  var yyyy = today.getFullYear();
+
+  today = yyyy + '-' + mm + '-' + dd;
+  document.getElementById("dueDate").setAttribute("min", today);
 }
 
 // Delete the task
@@ -50,24 +64,31 @@ function editTodo(index) {
   renderTask();
 }
 
-function searchFilterFunction(event){
-  console.log('event: ', event);
+function searchFilterFunction(event) {
+  const searchResultEmptyStateElement = document.getElementById('search-result-empty');
+  var matchFound = false;
   let searchText = event.target.value.toLowerCase();
+  if (!searchText) {
+    searchResultEmptyStateElement.style.display = 'none';
+  }
   let taskElementList = document.querySelector("ul");
   // li will have all the li elements inside the ul
   taskElements = taskElementList.getElementsByTagName("li");
-  console.log("li elements:",taskElements)
-  for ( let i = 0; i < taskArray.length; i++){
+
+  for (let i = 0; i < taskArray.length; i++) {
     textValue = taskArray[i].name;
     if (textValue.toLowerCase().indexOf(searchText) > -1) {
-      taskElements[i].style.display = "";
+      taskElements[i].style.display = "block";
+      matchFound = true;
     } else {
       taskElements[i].style.display = "none";
-      // taskElements[i].setAttribute("style", "display: none;");
     }
   }
-
+  if (!matchFound) {
+    searchResultEmptyStateElement.style.display = "block";
+  }
 }
+
 
 // Change the colour of the task if clicked on the compelted toggle box
 
@@ -127,14 +148,15 @@ function renderTask() {
                     <hr width="100%" size="2">
 
                     <div class = "block">
-                    <button class="btn"  onclick="editTodo(${index})"> <i class="fa fa-edit"></i> Edit </button>
-                    <button class="btn" onclick="deleteTodo(${index})"> <i class="fa fa-trash"></i> Delete </button>
-                    <i class="fa fa-check-circle" style="font-size:30px;color:red" onclick="changeColor(${index})"></i>
+                    <button class="btn" style="background-color: #04AA6D;" onclick="editTodo(${index})"> <i class="fa fa-edit"></i> Edit </button>
+                    <button class="btn" style="background-color: red;" onclick="deleteTodo(${index})"> <i class="fa fa-trash"></i> Delete </button>
+                    <i class="fa fa-check-circle" style="font-size:30px;color:#04AA6D;" onclick="changeColor(${index})"></i>
                     </div>`;
 
     demo.appendChild(tasklist);
 
-    tasklist.id = "taskList";
+    tasklist.classList.add("task-items");
+
   });
   getDate();
 }
@@ -144,14 +166,17 @@ submitForm.addEventListener("submit", (e) => {
   const taskName = document.getElementById("task").value;
   const taskDescription = document.getElementById("description").value;
   const taskDueDate = document.getElementById("dueDate").value;
-  e.preventDefault();
+  
+  if (!taskName && !taskDescription && !taskDueDate){
+    alert("Please fill in this field.")
+  }
   const taskItems = {
     name: taskName,
     description: taskDescription,
     dueDate: taskDueDate,
   };
 
-  if(editIndex == -1){
+  if (editIndex == -1) {
     taskArray.push(taskItems);
   }
   else{
@@ -164,6 +189,24 @@ submitForm.addEventListener("submit", (e) => {
 
   localStorage.setItem("taskItems", JSON.stringify(taskArray));
 
+  closeForm();
+
   renderTask();
 });
 renderTask();
+
+function handleSearchZeroState() {
+  const inputText = document.getElementById('inputtext').value;
+  const searchResultEmptyStateElement = document.getElementById('search-result-empty');
+  // if there are any elements satisfying search - show filtered list
+  let isSearchHasResults = taskArray.some((task) => {
+    return task.name.indexOf(inputtext) > -1;
+  });
+
+  if (!!isSearchHasResults && localStorage.getItem("taskItems") !== null) {
+    document.getElementById('zero-state-container').style.display = "none";
+  } else {
+    document.getElementById('zero-state-container').style.display = " ";
+  }
+}
+
